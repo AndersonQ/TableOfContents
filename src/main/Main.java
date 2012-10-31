640a56b7a1a414aece81f99bd251c56f5383539e
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-10-31 00:16:43 -0200

    More tests

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,21 @@
 package main;
 
+import hash.HashTable;
+
 import java.io.IOException;
+import java.util.Vector;
 
 import reader.Reader;
+import tree.trie.TrieTree;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException
 	{
+		TrieTree trie;
+		HashTable hash;
 		Reader reader;
+		Vector<String> keys;
 		//Checking parameters
 		if(args.length != 1)
 		{
@@ -18,5 +25,25 @@ public class Main {
 		
 		reader = new Reader(args[0]);
 		reader.readFile();
+		reader.searchKeyWords();
+		
+		trie = reader.getTrie();
+		hash = reader.getHash();
+		keys = trie.getAll();
+		System.out.println(keys);
+		trie.print();
+		System.out.println("\n-----------------" + keys.size());
+		for(int i = 0; i < keys.size(); i++)
+		{
+			int[] lines;
+			System.out.println("Searching: " + keys.get(i));
+			lines = hash.get(keys.get(i));
+			System.out.println("lines: " + lines);
+			System.out.print(keys.get(i) + ": ");
+			for(int l: lines)
+			{
+				System.out.print(l + ", ");
+			}
+		}
 	}
 }
